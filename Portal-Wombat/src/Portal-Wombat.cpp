0f4ab07db0da7e593b43a718d6b1828d34311378
frankakeAKA <frankakeAKA@googlemail.com>
2012-03-20 16:38:25 +0100

    second commit of doom!

---
+++ Portal-Wombat/src/Portal-Wombat.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Portal-Wombat.cpp
+// Author      : zodiac
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
